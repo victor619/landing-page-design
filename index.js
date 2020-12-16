20137f4979809db5f375e5bfe1384b97b50966f6
victor619 <victor619@github.com>
2020-12-16 16:06:11 +0100

    I added a little animation

---
+++ index.js
@@ -0,0 +1,38 @@
+// const bars = document.getElementsByClassName('bars')
+// const navItems = document.getElementsByClassName('nav-items')
+// const bar = document.getElementsByClassName('rotate')
+
+// bars[0].addEventListener('click',toggle)
+
+// function toggle(){
+//     if(window.screen.width  < 1400){
+//         if(navItems[0].style.display == 'none'){
+//             navItems[0].style.display = 'block'
+//         for (let i = 0; i < bar.length; i++) {
+//             bar[i].style.borderColor = 'navy'
+//             bar[i].style.position = 'absolute'
+//           }
+//         bar[0].style.transform = 'rotate(45deg)'
+//         bar[1].style.display = 'none'
+//         bar[2].style.transform = 'rotate(-45deg)'
+//         }
+
+//      else{
+//             navItems[0].style.display = 'none'
+//             for (let i = 0; i < bar.length; i++) {
+//                 bar[i].style.borderColor = 'grey'
+//                 bar[i].style.position = 'relative'
+//             }
+
+//         bar[0].style.transform = 'rotate(0deg)'
+//         bar[1].style.display = 'block'
+//         bar[2].style.transform = 'rotate(0deg)'
+//         }
+//     }
+//     else{
+//         console.log('false')
+//     }
+// }
+
+
+
